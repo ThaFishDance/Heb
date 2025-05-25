3222e2755579bbfe8568ca55921ae21a9f5c5820
ThaFishDance <chuffman91@gmail.com>
2025-05-25 15:25:33 -0500

    initial set up

---
+++ src/main/java/com/tcb/homedepot/HomeDepotApplication.java
@@ -0,0 +1,13 @@
+package com.tcb.homedepot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeDepotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeDepotApplication.class, args);
+    }
+
+}
