25b5011e8eb3cf65291331fe6e7773c6faedb10d
ThaFishDance <chuffman91@gmail.com>
2025-06-06 10:09:54 -0500

    update v1

--- src/main/resources/db/migration/V1__initial_migration.sql
+++ src/main/resources/db/migration/V1__initial_migration.sql
@@ -3,8 +3,8 @@ create table users
     id       bigint auto_increment
         primary key,
     name     varchar(255) not null,
-    email    varchar(255) null,
-    password varchar(255) null
+    email    varchar(255) not null,
+    password varchar(255) not null
 );
 
 create table addresses
