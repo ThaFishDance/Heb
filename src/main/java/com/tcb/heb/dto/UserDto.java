aa1f15ad7d9b78c20bca2058325b5579dd519805
ThaFishDance <chuffman91@gmail.com>
2025-06-09 13:45:28 -0500

    map id for user to user_id

--- src/main/java/com/tcb/heb/dto/UserDto.java
+++ src/main/java/com/tcb/heb/dto/UserDto.java
@@ -1,11 +1,13 @@
 package com.tcb.heb.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    @JsonProperty("user_id")
     public Long id;
     public String name;
     public String email;
