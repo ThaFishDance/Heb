091ae12054de703154b736b34b2be4a4652a72c5
ThaFishDance <chuffman91@gmail.com>
2025-06-09 16:24:31 -0500

    remove calls in main

--- src/main/java/com/tcb/heb/HebApplication.java
+++ src/main/java/com/tcb/heb/HebApplication.java
@@ -2,6 +2,7 @@ package com.tcb.heb;
 
 import com.tcb.heb.entities.Address;
 import com.tcb.heb.entities.User;
+import com.tcb.heb.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,26 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HebApplication {
 
     public static void main(String[] args) {
-//        var context = SpringApplication.run(HebApplication.class, args);
-        var user = User
-            .builder()
-            .name("John")
-            .password("password")
-            .email("test@test.com")
-            .build();
-
-        var address = Address
-            .builder()
-            .street("Street")
-            .city("City")
-            .state("State")
-            .zip("Zip")
-            .build();
-
-        user.addAddress(address);
-        System.out.println(user);
-
-
+        SpringApplication.run(HebApplication.class, args);
     }
 
 }
