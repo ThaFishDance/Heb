0b9ba8af5a837cf4e6edcefd89c0e4b303fab4ba
ThaFishDance <chuffman91@gmail.com>
2025-06-09 10:27:25 -0500

    update user controller with dynamic

--- src/main/java/com/tcb/heb/controllers/UserController.java
+++ src/main/java/com/tcb/heb/controllers/UserController.java
@@ -3,17 +3,32 @@ package com.tcb.heb.controllers;
 import com.tcb.heb.entities.User;
 import com.tcb.heb.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor // Lombok for userRepository
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getAllUsers() {
         return userRepository.findAll(); // returns an iterable not a list
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(user);
+    }
+
 }
