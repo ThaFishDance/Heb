3844ffe382a66bd8b0bf7278815ee41ce8b66b50
ThaFishDance <chuffman91@gmail.com>
2025-06-09 16:02:23 -0500

    create post mapping

--- src/main/java/com/tcb/heb/controllers/UserController.java
+++ src/main/java/com/tcb/heb/controllers/UserController.java
@@ -5,10 +5,7 @@ import com.tcb.heb.mappers.UserMapper;
 import com.tcb.heb.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,6 +33,11 @@ public class UserController {
         }
 
         return ResponseEntity.ok(userMapper.toDto(user));
+    }
+
+    @PostMapping
+    public UserDto createUser(@RequestBody UserDto data) {
+        return data;
     }
 
 }
