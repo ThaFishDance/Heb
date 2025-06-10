ae76e64a37c6ae27c7e72e8ad6757010c9d33294
ThaFishDance <chuffman91@gmail.com>
2025-06-10 13:03:47 -0500

    clean up cinfig and fix bug for cart date created

--- src/main/java/com/tcb/heb/entities/CartItem.java
+++ src/main/java/com/tcb/heb/entities/CartItem.java
@@ -4,9 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 @Getter
 @Setter
@@ -15,24 +12,20 @@ import org.hibernate.annotations.OnDeleteAction;
 public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+    @Column(name = "id")
     private Long id;
 
-    @NotNull
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @JoinColumn(name = "cart_id", nullable = false)
+
+    @ManyToOne()
+    @JoinColumn(name = "cart_id")
     private Cart cart;
 
     @NotNull
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @JoinColumn(name = "product_id", nullable = false)
+    @ManyToOne()
+    @JoinColumn(name = "product_id")
     private Product product;
 
-    @NotNull
-    @ColumnDefault("1")
-    @Column(name = "quantity", nullable = false)
+    @Column(name = "quantity")
     private Integer quantity;
 
 }
