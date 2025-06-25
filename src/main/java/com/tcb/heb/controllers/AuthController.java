10af794c1e0879b880eb72cf5447f9ab868c17c1
ThaFishDance <chuffman91@gmail.com>
2025-06-25 13:45:40 -0500

    introduction of Authentication Manager

--- src/main/java/com/tcb/heb/controllers/AuthController.java
+++ src/main/java/com/tcb/heb/controllers/AuthController.java
@@ -1,50 +1,35 @@
 package com.tcb.heb.controllers;
 
 import com.tcb.heb.dto.UserLoginRequest;
-import com.tcb.heb.entities.User;
-import com.tcb.heb.mappers.UserMapper;
-import com.tcb.heb.repositories.UserRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 @Tag(name = "auth")
 @AllArgsConstructor
 public class AuthController {
-
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
-    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(
         @Valid @RequestBody UserLoginRequest request
     ) {
 
-        // Check for email in DB
-        User user = userRepository.findUserByEmail(request.getEmail()).orElse(null);
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                Map.of("error", "User not found")
-            );
-        }
-
-        // Check if passwords match
-        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+            )
+        );
 
         // Return Success
         return ResponseEntity.ok().build();
