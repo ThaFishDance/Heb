b2ee8d85176eb768664ea6600e46a630a2e41784
ThaFishDance <chuffman91@gmail.com>
2025-06-25 13:48:17 -0500

    handle cad credentials

--- src/main/java/com/tcb/heb/controllers/AuthController.java
+++ src/main/java/com/tcb/heb/controllers/AuthController.java
@@ -4,13 +4,13 @@ import com.tcb.heb.dto.UserLoginRequest;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -33,6 +33,11 @@ public class AuthController {
 
         // Return Success
         return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler({BadCredentialsException.class})
+    public ResponseEntity<Void> handleBadCredentialsException(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
 }
