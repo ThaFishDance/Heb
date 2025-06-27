1be331039a700010ab0574523549401427a5a8c8
ThaFishDance <chuffman91@gmail.com>
2025-06-27 09:17:25 -0500

    managing secrets

--- src/main/java/com/tcb/heb/services/JwtService.java
+++ src/main/java/com/tcb/heb/services/JwtService.java
@@ -12,6 +12,9 @@ import java.util.Date;
 
 @Service
 public class JwtService {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
     public String generateToken(String email) {
         final long tokenExpiration = 86400; // 1 day
 
@@ -19,7 +22,7 @@ public class JwtService {
             .subject(email)
             .issuedAt(new Date())
             .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-            .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+            .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
             .compact();
     }
 }
