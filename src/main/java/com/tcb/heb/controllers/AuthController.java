fc62691f731e3a637720ac716f6187ca9e9a17ad
ThaFishDance <chuffman91@gmail.com>
2025-06-27 10:13:45 -0500

    accessing current user

--- src/main/java/com/tcb/heb/controllers/AuthController.java
+++ src/main/java/com/tcb/heb/controllers/AuthController.java
@@ -2,6 +2,9 @@ package com.tcb.heb.controllers;
 
 import com.tcb.heb.dto.JwtResponse;
 import com.tcb.heb.dto.LoginRequest;
+import com.tcb.heb.dto.UserDto;
+import com.tcb.heb.mappers.UserMapper;
+import com.tcb.heb.repositories.UserRepository;
 import com.tcb.heb.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -10,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -18,6 +22,8 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(
@@ -39,6 +45,21 @@ public class AuthController {
         var token = authHeader.replace("Bearer ", "");
 
         return jwtService.validateToken(token);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+
+        var user = userRepository.findByEmail(email).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var userDto = userMapper.toDto(user);
+
+        return ResponseEntity.ok(userDto);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
